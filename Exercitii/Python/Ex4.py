28261a851a923c47d1f66372357d3ba8be74a38e
Arsenie-Vlad <147584006+Arsenie-Vlad@users.noreply.github.com>
2024-02-14 12:51:37 +0200

    Create Ex4.py

---
+++ Exercitii/Python/Ex4.py
@@ -0,0 +1,13 @@
+# Scrieţi un program care citeşte două numere naturale şi tipăreşte suma,
+# diferenţa, produsul şi câtul lor întreg.
+
+n=int(input('n='));
+m=int(input('m='));
+s=n+m;
+dif=n-m;
+p=n*m;
+cat=n//m;
+print('Suma:', s);
+print('Diferenta:', dif);
+print('Produsul:', p);
+print('Catul:', cat);
